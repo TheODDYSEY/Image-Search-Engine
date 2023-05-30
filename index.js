a86f95cca3311ddec929d50101ebb8a4ac11d0f9
TheODDYSEY <mosessabz07@gmail.com>
2023-05-30 18:45:55 +0300

    First Iteration of Project

---
+++ index.js
@@ -0,0 +1,69 @@
+import weaviate from 'weaviate-ts-client';
+
+const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+});
+
+const schemaRes = await client.schema.getter().do();
+
+console.log(schemaRes)
+// create a schema
+
+const schemaConfig = {
+    'class': 'Meme',
+    'vectorizer': 'img2vec-neural',
+    'vectorIndexType': 'hnsw',
+    'moduleConfig': {
+        'img2vec-neural': {
+            'imageFields': [
+                'image'
+            ]
+        }
+    },
+    'properties': [
+        {
+            'name': 'image',
+            'dataType': ['blob']
+        },
+        {
+            'name': 'text',
+            'dataType': ['string']
+        }
+    ]
+}
+
+await client.schema
+    .classCreator()
+    .withClass(schemaConfig)
+    .do();
+
+
+//  store an image   
+const { readFileSync } = require('fs');
+
+ const img = readFileSync('./img/hi-mom.jpg');
+
+const b64 = Buffer.from(img).toString('base64');
+
+await client.data.creator()
+  .withClassName('Meme')
+  .withProperties({
+    image: b64,
+    text: 'matrix meme'
+  })
+  .do();
+
+//   query an image
+const test = Buffer.from( readFileSync('./matrix.jpg') ).toString('base64');
+
+const resImage = await client.graphql.get()
+  .withClassName('Meme')
+  .withFields(['image'])
+  .withNearImage({ image: test })
+  .withLimit(1)
+  .do();
+
+// Write result to filesystem
+const result = resImage.data.Get.Meme[0].image;
+writeFileSync('./result.jpg', result, 'base64');
